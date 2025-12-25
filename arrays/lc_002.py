2511b67e0e53c19b241b87e2e067606063d7b1c5
Umrii <aatiq145@gmail.com>
2025-12-25 22:33:16 +0000

    LC_002

--- arrays/lc_002.py
+++ arrays/lc_002.py
@@ -1,5 +1,6 @@
 # Best time to buy and sell stocks
 # Finding the minimium first to start finding the maximum after it
+# First minimun would serve as the best time to buy stocks
 
 
 prices = [7,1,5,3,6,0]
