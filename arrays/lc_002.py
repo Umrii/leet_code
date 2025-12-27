a14007ab5b1b76b4a56bdfafd5b5191c9600c217
Umrii <aatiq145@gmail.com>
2025-12-27 22:02:49 +0000

    LC_002

--- arrays/lc_002.py
+++ arrays/lc_002.py
@@ -1,26 +1,11 @@
 # Best time to buy and sell stonks
 
+def minmax():    
+    prices =[7,6,4,3,1]
+    arr_profit=[]
+    for i in range(len(prices)):
+        for j in range(i+1,len(prices)):
+            
+            profit=prices[j]-prices[i]
 
-prices = [7,6,4,3,1]
-# print(len(prices)-1)
-
-min=prices[0]
-temp=0
-day=0
-
-for i in range(len(prices)-2):
-        if min > prices[i+1]:
-            min=prices[i+1]
-            day=i+1
-print(day,min)
-max=0
-day2=0
-for i in range(day+1,len(prices)-1):
-    # print(i)
-    if max < prices[i]:
-        max=prices[i]
-        day2=i
-print(day2,max)
-
-
-
+print(minmax())
