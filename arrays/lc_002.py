71cba661a2623ceb07fbaa8b49e71cf9973b9507
Umrii <aatiq145@gmail.com>
2025-12-27 22:04:39 +0000

    LC_002

--- arrays/lc_002.py
+++ arrays/lc_002.py
@@ -2,10 +2,17 @@
 
 def minmax():    
     prices =[7,6,4,3,1]
-    arr_profit=[]
+    
+    
+
     for i in range(len(prices)):
         for j in range(i+1,len(prices)):
             
             profit=prices[j]-prices[i]
+            if last_profit > profit:
+                pass
+            
+            last_profit=profit
+            
 
 print(minmax())
