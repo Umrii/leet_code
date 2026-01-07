d221d76b5e6fdfeb3a661b73f9bc7fcdc8a0d9f5
Umrii <aatiq145@gmail.com>
2026-01-07 08:54:53 +0000

    LC_004

--- arrays/lc_004.py
+++ arrays/lc_004.py
@@ -1,13 +1,13 @@
-# nums = [0,1,0,3,12]
+#Shift Zeros to the Right
+nums = [0,1,0,3,12]
 
-# for i in range(len(nums)):
-#     # for j in range(i,len(nums)):
-#         print(i,i+1)
-#         # if nums[i]==0:
-#         #     temp=nums[j]
-#         #     nums[j]=nums[i]
-#         #     nums[i]=temp
+for i in range(len(nums)):
+    for j in range(i,len(nums)):
+        if nums[i]==0:
+            temp=nums[j]
+            nums[j]=nums[i]
+            nums[i]=temp
 
 
-# # print(nums)
+print(nums)
 
